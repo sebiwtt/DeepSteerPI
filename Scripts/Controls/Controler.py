27727f62b47e361a9e0a11bebb0bae4d4be098b8
Sebastian Wette <Sebastian.wette@telekom.de>
2024-06-12 10:28:11 +0200

    enabled switch for collection mode

--- Scripts/Controls/Controler.py
+++ Scripts/Controls/Controler.py
@@ -32,7 +32,7 @@ try:
         events = get_gamepad()
         for event in events:
             if event.ev_type == "Key" and event.code == "BTN_SOUTH" and event.state == 1:  # X-Button
-                #collecting_data = not collecting_data
+                collecting_data = not collecting_data
                 if collecting_data:
                     print("Data collection started")
                 else:
