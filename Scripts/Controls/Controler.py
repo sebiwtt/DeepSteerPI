6425b66e375762901fc5c22876e87c5272874b1e
Sebastian Wette <Sebastian.wette@telekom.de>
2024-06-12 10:23:08 +0200

    update to picam2

--- Scripts/Controls/Controler.py
+++ Scripts/Controls/Controler.py
@@ -3,15 +3,13 @@ import csv
 import time
 from inputs import get_gamepad
 from datetime import datetime
-#from picamera import PiCamera
+from picamera2 import Picamera2, Preview
 import L298NHBridge as HBridge
 
-# Helper to make talking to the motors easy
 def set_motor_speeds(left_speed, right_speed):
     HBridge.setMotorLeft(left_speed)
     HBridge.setMotorRight(right_speed)
 
-# Create a directory to save images and a csv file to write the raw data
 if not os.path.exists('training_data'):
     os.makedirs('training_data')
 
@@ -19,13 +17,15 @@ csv_file = open('training_data/data_log.csv', 'w', newline='')
 csv_writer = csv.writer(csv_file)
 csv_writer.writerow(['timestamp', 'image_path', 'left_speed', 'right_speed', 'steering_angle'])
 
-# Initialize variables
 collecting_data = False
 speed = 0
 steering_angle = 0
 running = True
-#camera = PiCamera()
-#camera.resolution = (640, 480)
+
+picam2 = Picamera2()
+config = picam2.create_still_configuration()
+picam2.configure(config)
+picam2.start()
 
 try:
     while running:
@@ -61,13 +61,12 @@ try:
             print(f"RightSpeed: {right_motor_speed}") 
             print(f"LeftSpeed: {left_motor_speed}") 
 
-            # Capture and save image, write log-data to csv
             if collecting_data:
                 timestamp = datetime.now().strftime('%Y%m%d_%H%M%S%f')
                 image_path = f'training_data/{timestamp}.jpg'
-                camera.capture(image_path)
-                csv_writer.writerow([timestamp, image_path, left_motor_speed, right_motor_speed, steering_angle])
-                time.sleep(0.1)  
+                picam2.capture_file(image_path)
+                csv_writer.writerow([timestamp, image_path, speed, steering_angle])
+                time.sleep(0.1)  # Adjust the sleep duration as necessary 
 
 except KeyboardInterrupt:
     print("Data collection interrupted")
@@ -76,5 +75,5 @@ finally:
     HBridge.setMotorLeft(0)
     HBridge.setMotorRight(0)
     HBridge.exit()
-    #camera.close()
-    #csv_file.close()
+    picam2.stop()
+    csv_file.close()
