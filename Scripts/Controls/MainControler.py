eb12c2b9c209a784658140ca953425b1e93c7b9c
Sebastian Wette <sebastian.wette@web.de>
2024-06-28 09:23:13 +0200

    added led indicator and smaller image size

--- Scripts/Controls/MainControler.py
+++ Scripts/Controls/MainControler.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from picamera2 import Picamera2, Preview
 import L298NHBridge as HBridge
 import shutil
+from PIL import Image
 
 #--------- Helper Functions ---------
 
@@ -56,8 +57,17 @@ def collect_data():
                 continue
 
             timestamp = datetime.now().strftime('%Y%m%d_%H%M%S%f')
+            temp_image_path = "training_data/temp.jpg"
+            picam2.capture_file(temp_image_path)
+
             image_path = f'training_data/{timestamp}.jpg'
-            picam2.capture_file(image_path)
+
+            image = Image.open(temp_image_path)
+            image = image.resize((820,616), Image.ANTIALIAS)
+            image.save(image_path, format="JPEG", quality=85)
+
+            os.remove(temp_image_path)
+
             #print(f'Captured image: {image_path}')
             csv_writer.writerow([timestamp, image_path, controller_state['left_stick'], controller_state['right_stick']])
             #print(f'Wrote data to csv file: {timestamp}, {image_path}, {controller_state["left_stick"]}, {controller_state["right_stick"]}')
@@ -68,6 +78,16 @@ def collect_data():
 def check_disk_space():
     total, used, free = shutil.disk_usage("/")
     return free
+
+LED_PATH = "/sys/class/leds/led0/brightness"
+
+def led_on():
+    with open(LED_PATH, "w") as f:
+        f.write("1")
+
+def led_off():
+    with open(LED_PATH, "w") as f:
+        f.write("0")
 
 #--------- Global Variables ---------
 
@@ -111,7 +131,9 @@ try:
                 collecting_data = not collecting_data
                 if collecting_data:
                     print("Data collection started")
+                    led_on()
                 else:
+                    led_off()
                     print("Data collection stopped")
 
             elif event.ev_type == "Key" and event.code == "BTN_WEST" and event.state == 1: # Square-Button
